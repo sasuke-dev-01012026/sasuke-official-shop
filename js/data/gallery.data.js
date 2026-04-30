9f8a5aef9ed2c5e61a931cfa55b57bdc5d340928
sasuke-dev-01012026 <brothercloth34@gmail.com>
2026-04-30 15:50:35 +0700

    Update gallery.data.js

--- js/data/gallery.data.js
+++ js/data/gallery.data.js
@@ -30,7 +30,11 @@ export const GALLERY_IMGS = [
     './assets/images/gallery/naruto/02.jpg',
     './assets/images/gallery/naruto/03.jpg',
   ],
-  './assets/images/gallery/dragonball/01.png',               // Goku
+  [                                                           // Dragon Ball (3 gambar)
+    './assets/images/gallery/dragonball/01.png',
+    './assets/images/gallery/dragonball/02.png',
+    './assets/images/gallery/dragonball/03.png',
+  ],
   './assets/images/gallery/bleach/01.jpg',                   // Ichigo
   './assets/images/gallery/fairy_tail/01.png',               // Natsu
   './assets/images/gallery/other/01.png',                    // Tanjiro
